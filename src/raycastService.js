37f1b8a45d9522760348253195280efc9e0d3ad1
turner <douglass.turner@gmail.com>
2025-05-01 11:50:59 -0400

    Give raycast visual feedback sphere a name

--- src/raycastService.js
+++ src/raycastService.js
@@ -47,6 +47,7 @@ class RayCastService {
 
     createVisualFeeback(color) {
 		const sphere = new THREE.Mesh(new THREE.SphereGeometry(16, 32, 16), new THREE.MeshBasicMaterial({ color, depthTest: false }))
+		sphere.name = 'raycastVisualFeedback'
 		sphere.visible = false
 		sphere.renderOrder = 10
 		return sphere
