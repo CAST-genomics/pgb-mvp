2157c992a4b989d3f8ece2d0cb4b860bd7de21d3
turner <douglass.turner@gmail.com>
2025-05-14 09:23:57 -0400

    Move glsl code to individual files

--- src/materialLibrary.js
+++ src/materialLibrary.js
@@ -1,37 +1,6 @@
 import * as THREE from 'three';
-
-// Shader code for the gradient effect
-const vertexShader = `
-varying vec2 vUv;
-void main() {
-    vUv = uv;
-    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-}
-`;
-
-const fragmentShader = `
-uniform vec3 startColor;
-uniform vec3 endColor;
-uniform sampler2D map;
-uniform sampler2D gradientMap;
-uniform vec2 uvOffset;
-varying vec2 vUv;
-
-void main() {
-    // Sample the gradient texture
-    vec4 gradient = texture2D(gradientMap, vUv);
-    
-    // Sample the arrow texture with animation
-    vec2 animatedUv = vUv + uvOffset;
-    vec4 arrow = texture2D(map, animatedUv);
-    
-    // Mix the colors based on the gradient
-    vec3 mixedColor = mix(startColor, endColor, gradient.r);
-    
-    // Combine with the arrow texture
-    gl_FragColor = vec4(mixedColor, arrow.a);
-}
-`;
+import vertexShader from '../shaders/animated-arrow.vert.glsl';
+import fragmentShader from '../shaders/animated-arrow.frag.glsl';
 
 /**
  * Creates a basic material for an arrow with a single color tint.
