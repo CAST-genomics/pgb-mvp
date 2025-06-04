4f5218ab33127d27c0e988556139e6c81cc3e336
turner <douglass.turner@gmail.com>
2025-06-04 15:02:11 -0400

    update igvCore

--- src/sequenceService.js
+++ src/sequenceService.js
@@ -82,7 +82,8 @@ class SequenceService {
     repaint() {
         if (!this.currentNodeName) return;
 
-        const sequence = this.genomicService.metadata.get(this.currentNodeName).sequence;
+        const payload = this.genomicService.metadata.get(this.currentNodeName);
+        const { sequence } = payload
 
         if (!sequence) {
             console.error(`No sequence found for ${this.currentNodeName}`);
